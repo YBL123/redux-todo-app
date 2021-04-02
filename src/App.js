738cd299781c2626b0da195710aef01cd02da3f2
Yarden Lawson <yardenlawson@gmail.com>
2021-04-02 12:43:16 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* todo list */}
+      {/* done */}
+      {/* input */}
+    </div>
+  );
+}
+
+export default App;
